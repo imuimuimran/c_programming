2b8078e76bbfe58520ff9feb36768aa4e3489efe
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-05-16 23:04:10 +0600

    This C program can divide by 2 also it will be even both of case

---
+++ basic_practice/divided_by_2_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // Input an integer where the number can be divide by 2 also it will be even both of case
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    if(num%2==0 && num!=2)
+    {
+        printf("Yes, it can be divided with two even number.");
+    }
+    else
+    {
+        printf("Sorry, it can't be divide with two even number.");
+    }
+
+    return 0;
+}
